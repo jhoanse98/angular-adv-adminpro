7bdb648544a73f76a57036c1f49de0e090ad91e4
Sebastian Marin <sebastian.marin.val@gmail.com>
2021-02-13 17:39:24 -0500

    seccion 7 finalizada: servicios, rutas y persistencia en los ajustes

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { SettingsService } from '../services/settings.service';
 
+declare function customInitFunction();
+
 @Component({
   selector: 'app-pages',
   templateUrl: './pages.component.html',
@@ -13,7 +15,7 @@ export class PagesComponent implements OnInit {
   constructor(private SettingsService:SettingsService) { }
 
   ngOnInit(): void {
-    
+    customInitFunction()
   }
 
 }
